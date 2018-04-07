c14d69a2799174f24150800ab06a686a9625a4c3
Vadim Shmakov <shmakovvadim@mail.ru>
2018-04-07 10:23:54 +0500

    Added solve and answer privs to AttemptVoter

--- _smf3/Controller/AttemptVoter.php
+++ _smf3/Controller/AttemptVoter.php
@@ -11,6 +11,7 @@ use App\Service\UserLoader;
 class AttemptVoter extends Voter
 {
 private $ul;
+private $att;
 
 public function __construct(UserLoader $ul) {
 $this->ul=$ul;
@@ -23,7 +24,15 @@ $this->ul=$ul;
 
     protected function voteOnAttribute($attribute, $subject, TokenInterface $token)
     {
+$this->att=$subject;
+
         switch ($attribute) {
+case "SOLVE":
+return $this->canSolve();
+break;
+case "ANSWER":
+return $this->canAnswer();
+break;
             case "EDIT":
 return true;
                 break;
@@ -34,5 +43,13 @@ return true;
 
         return false;
     }
+
+private function canSolve() {
+
+}
+
+private function canAnswer() {
+
+}
 
 }

b630f772f92cc9cde85445e3e9b2dd691235302d
Vadim Shmakov <shmakovvadim@mail.ru>
2018-04-07 12:24:30 +0500

    Tried to fix bug with templating service in FOSUserBundle

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -1,13 +1,13 @@
 <?php
 
 namespace App\Entity;
-
+use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
  */
-class User
+class User extends BaseUser
 {
     /**
      * @ORM\Id()

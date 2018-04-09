6c07f97b6ece1acb628cc2106aeae54090a5c273
Vadim Shmakov <shmakovvadim@mail.ru>
2018-04-09 17:59:19 +0500

    Generated Profile CRUD

---
+++ src/Form/ProfileType.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Profile;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProfileType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('addTime')
+            ->add('isPublic')
+            ->add('duration')
+            ->add('examplesCount')
+            ->add('addMin')
+            ->add('addMax')
+            ->add('subMin')
+            ->add('subMax')
+            ->add('minSub')
+            ->add('multMin')
+            ->add('multMax')
+            ->add('divMin')
+            ->add('divMax')
+            ->add('minDiv')
+            ->add('addPerc')
+            ->add('subPerc')
+            ->add('multPerc')
+            ->add('divPerc')
+            ->add('author')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Profile::class,
+        ]);
+    }
+}

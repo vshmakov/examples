430941eff3cd1500b75d5bd3a5b45cca03ea7a60
Vadim Shmakov <shmakovvadim@mail.ru>
2018-04-24 22:56:42 +0500

    Created Code  CRUD

---
+++ src/Controller/CodeController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Code;
+use App\Form\CodeType;
+use App\Repository\CodeRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/admin/code")
+ */
+class CodeController extends Controller
+{
+    /**
+     * @Route("/", name="code_index", methods="GET")
+     */
+    public function index(CodeRepository $codeRepository): Response
+    {
+        return $this->render('code/index.html.twig', ['codes' => $codeRepository->findAll()]);
+    }
+
+    /**
+     * @Route("/new", name="code_new", methods="GET|POST")
+     */
+    public function new(Request $request): Response
+    {
+        $code = new Code();
+        $form = $this->createForm(CodeType::class, $code);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($code);
+            $em->flush();
+
+            return $this->redirectToRoute('code_index');
+        }
+
+        return $this->render('code/new.html.twig', [
+            'code' => $code,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="code_show", methods="GET")
+     */
+    public function show(Code $code): Response
+    {
+        return $this->render('code/show.html.twig', ['code' => $code]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="code_edit", methods="GET|POST")
+     */
+    public function edit(Request $request, Code $code): Response
+    {
+        $form = $this->createForm(CodeType::class, $code);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('code_edit', ['id' => $code->getId()]);
+        }
+
+        return $this->render('code/edit.html.twig', [
+            'code' => $code,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="code_delete", methods="DELETE")
+     */
+    public function delete(Request $request, Code $code): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$code->getId(), $request->request->get('_token'))) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($code);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('code_index');
+    }
+}
---
+++ src/Form/CodeType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Code;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CodeType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('string')
+            ->add('addTime')
+            ->add('activateTime')
+            ->add('activated')
+            ->add('user')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Code::class,
+        ]);
+    }
+}

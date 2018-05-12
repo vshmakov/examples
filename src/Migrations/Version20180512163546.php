d26f76834997e774fff2c5f65e60a059eaee838e
Vadim Shmakov <shmakovvadim@mail.ru>
2018-05-12 21:39:05 +0500

    Updated dependencies

--- src/Migrations/Version20180512163546.php
+++ src/Migrations/Version20180512163546.php
@@ -1,4 +1,4 @@
-<?php declare(strict_types = 1);
+<?php declare(strict_types=1);
 
 namespace DoctrineMigrations;
 
@@ -8,9 +8,9 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version20180424180809 extends AbstractMigration
+final class Version20180512163546 extends AbstractMigration
 {
-    public function up(Schema $schema)
+    public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
@@ -20,7 +20,7 @@ class Version20180424180809 extends AbstractMigration
         $this->addSql('CREATE TABLE example (id INT AUTO_INCREMENT NOT NULL, attempt_id INT NOT NULL, first DOUBLE PRECISION NOT NULL, sign SMALLINT NOT NULL, second DOUBLE PRECISION NOT NULL, answer DOUBLE PRECISION DEFAULT NULL, is_right TINYINT(1) DEFAULT NULL, add_time DATETIME NOT NULL, answer_time DATETIME DEFAULT NULL, INDEX IDX_6EEC9B9FB191BE6B (attempt_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('CREATE TABLE profile (id INT AUTO_INCREMENT NOT NULL, author_id INT NOT NULL, add_time DATETIME NOT NULL, is_public TINYINT(1) NOT NULL, duration SMALLINT NOT NULL, examples_count SMALLINT NOT NULL, add_min INT NOT NULL, add_max INT NOT NULL, sub_min INT NOT NULL, sub_max INT NOT NULL, min_sub INT NOT NULL, mult_min INT NOT NULL, mult_max INT NOT NULL, div_min INT NOT NULL, div_max INT NOT NULL, min_div INT NOT NULL, add_perc SMALLINT NOT NULL, sub_perc SMALLINT NOT NULL, mult_perc SMALLINT NOT NULL, div_perc SMALLINT NOT NULL, description VARCHAR(255) NOT NULL, is_demanding TINYINT(1) NOT NULL, INDEX IDX_8157AA0FF675F31B (author_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('CREATE TABLE session (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, add_time DATETIME NOT NULL, sid VARCHAR(32) NOT NULL, INDEX IDX_D044D5D4A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, profile_id INT DEFAULT NULL, username VARCHAR(180) NOT NULL, username_canonical VARCHAR(180) NOT NULL, email VARCHAR(180) NOT NULL, email_canonical VARCHAR(180) NOT NULL, enabled TINYINT(1) NOT NULL, salt VARCHAR(255) DEFAULT NULL, password VARCHAR(255) NOT NULL, last_login DATETIME DEFAULT NULL, confirmation_token VARCHAR(180) DEFAULT NULL, password_requested_at DATETIME DEFAULT NULL, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:array)\', money SMALLINT NOT NULL, limit_time DATETIME NOT NULL, UNIQUE INDEX UNIQ_8D93D64992FC23A8 (username_canonical), UNIQUE INDEX UNIQ_8D93D649A0D96FBF (email_canonical), UNIQUE INDEX UNIQ_8D93D649C05FB297 (confirmation_token), INDEX IDX_8D93D649CCFA12B8 (profile_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, profile_id INT DEFAULT NULL, username VARCHAR(180) NOT NULL, username_canonical VARCHAR(180) NOT NULL, email VARCHAR(180) NOT NULL, email_canonical VARCHAR(180) NOT NULL, enabled TINYINT(1) NOT NULL, salt VARCHAR(255) DEFAULT NULL, password VARCHAR(255) NOT NULL, last_login DATETIME DEFAULT NULL, confirmation_token VARCHAR(180) DEFAULT NULL, password_requested_at DATETIME DEFAULT NULL, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:array)\', money SMALLINT NOT NULL, all_money INT NOT NULL, limit_time DATETIME NOT NULL, UNIQUE INDEX UNIQ_8D93D64992FC23A8 (username_canonical), UNIQUE INDEX UNIQ_8D93D649A0D96FBF (email_canonical), UNIQUE INDEX UNIQ_8D93D649C05FB297 (confirmation_token), INDEX IDX_8D93D649CCFA12B8 (profile_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('ALTER TABLE attempt ADD CONSTRAINT FK_18EC0266613FECDF FOREIGN KEY (session_id) REFERENCES session (id)');
         $this->addSql('ALTER TABLE code ADD CONSTRAINT FK_77153098A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
         $this->addSql('ALTER TABLE example ADD CONSTRAINT FK_6EEC9B9FB191BE6B FOREIGN KEY (attempt_id) REFERENCES attempt (id)');
@@ -29,7 +29,7 @@ class Version20180424180809 extends AbstractMigration
         $this->addSql('ALTER TABLE user ADD CONSTRAINT FK_8D93D649CCFA12B8 FOREIGN KEY (profile_id) REFERENCES profile (id)');
     }
 
-    public function down(Schema $schema)
+    public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');

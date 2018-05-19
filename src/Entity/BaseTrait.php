8e22062685ade9190c59814312c24cea2fd1abc9
Vadim Shmakov <shmakovvadim@mail.ru>
2018-05-19 15:38:15 +0500

    Added getSetter

--- src/Entity/BaseTrait.php
+++ src/Entity/BaseTrait.php
@@ -15,7 +15,21 @@ return $this;
 }
 
 public function __call($v, $p=[]) {
+if ($gs=$this->getSetter($v, $p)) return $gs["ret"];
 $m=entityGetter($v);
 return $this->er->$m($this);
 }
+
+private function getSetter($method, $p) {
+foreach (['get', 'set'] as $action) {
+if (preg_match("#^$action(.+)$#", $method, $arr)) {
+$v=lcfirst($arr[1]);
+if (!$g=$action == 'get') $this->$v=$p[0];
+return ["ret"=>$g ? $this->$v : $this];
+}
+}
+
+return false;
+}
+
 }

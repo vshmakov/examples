45bf397fbbbb2c78a9d9bff628b84a263d79a001
Vadim Shmakov <shmakovvadim@mail.ru>
2018-06-13 22:02:04 +0500

    Added ip session marking

--- src/Service/SessionMarker.php
+++ src/Service/SessionMarker.php
@@ -2,19 +2,28 @@
 
 namespace App\Service;
 
-use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\HttpFoundation\{
+Session\SessionInterface,
+RequestStack,
+};
 
 class SessionMarker {
 private $s;
+private $req;
 
-public function __construct(SessionInterface $s) {
+public function __construct(SessionInterface $s, RequestStack $rs) {
 $this->s=$s;
+$this->req=$rs->getMasterRequest();
 }
 
 public function getKey() {
+$ip=$this->req->getClientIp();
+//$val=$this->getRand();
+$val=$ip;
+
 $s=$this->s;
 $key="VISIT_KEY";
-if (!$s->has($key)) $s->set($key, $this->getRand());
+if (!$s->has($key)) $s->set($key, $val);
 $sid=$s->get($key);
 return $sid;
 }

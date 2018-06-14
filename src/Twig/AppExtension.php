63884c9dd2de25cc7e5a2c2019a8226ad9f8d66c
Vadim Shmakov <shmakovvadim@mail.ru>
2018-06-14 23:34:31 +0500

    Required Goutte Done IpInformer

--- src/Twig/AppExtension.php
+++ src/Twig/AppExtension.php
@@ -38,6 +38,7 @@ return $this->gl;
 public function getFunctions() {
 return [
 new TwigFunction("addTimeNumber", [$this, "getAddTimeNumber"]),
+new TwigFunction("getIpInfo", '\App\Service\IpInformer::getInfoByIp'),
 ];
 }
 

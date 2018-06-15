f825be30230663a26d36058ff256dd106699b5f4
Vadim Shmakov <shmakovvadim@mail.ru>
2018-06-15 13:33:18 +0500

    Fixed bugs

--- src/Migrations/Version20180615082006.php
+++ src/Migrations/Version20180615082006.php
@@ -8,14 +8,14 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20180615064438 extends AbstractMigration
+final class Version20180615082006 extends AbstractMigration
 {
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE user ADD ips LONGTEXT NOT NULL COMMENT \'(DC2Type:array)\'');
+        $this->addSql('ALTER TABLE user ADD ips LONGTEXT DEFAULT NULL COMMENT \'(DC2Type:array)\'');
     }
 
     public function down(Schema $schema) : void

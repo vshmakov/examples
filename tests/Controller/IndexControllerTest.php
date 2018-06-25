3d1c6f5227a438401e42a60f7f1e27a30e0795fe
Vadim Shmakov <shmakovvadim@mail.ru>
2018-06-25 16:01:12 +0500

    Added api_request_yandex test

---
+++ tests/Controller/IndexControllerTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class IndexControllerTest extends WebTestCase
+{
+    public function testRequest()
+    {
+        $client = static::createClient();
+
+        $client->request('POST', '/api/request/yandex');
+
+        $this->assertEquals(400, $client->getResponse()->getStatusCode());
+    }
+}

d87cfead0acc576c7b7061870ebc5dc90dd594b0
Vadim Shmakov <shmakovvadim@mail.ru>
2018-07-02 00:08:48 +0500

    Added admin_index action

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -12,21 +12,34 @@ AttemptRepository as AttR,
 class AdminController extends Controller
 {
     /**
-     * @Route("/admin", name="admin")
+     * @Route("/admin", name="admin_index")
      */
     public function index(IpR $ipR, AttR $attR)
     {
-$d=[
-"ipC"=>$ipR->q("select i from App:Ip i
-where i.addTime > :dt")
-        ];
+$d=[];
+$d0=[
+"ipC"=>"select count(i) from App:Ip i
+where i.addTime > :dt",
+"visits"=>"select count(v) from App:Visit v
+join v.session s
+where s.addTime > :dt",
+"attempts"=>"select count(a) from App:Attempt a
+join a.session s
+where s.addTime > :dt",
+"users"=>"select count(u) from App:User u
+where u.addTime > :dt",
+      ];
 
-foreach ($d as $k=>$v) {
-$d[$k]=$ipR->v(
-$v->setParameter("dt", (new \DateTime)->sub(new \DateInterval("P1D")))
+foreach ($d0 as $k=>$v) {
+foreach ([1, 7, 28] as $t) {
+$d[$t][$k]=$ipR->v(
+$ipR->q($v)->setParameter("dt", (new \DateTime)->sub(new \DateInterval("P{$t}D")))
 );
 }
+}
 
-        return $this->render('admin/index.html.twig', $d);
+        return $this->render('admin/index.html.twig', [
+"d"=>$d
+]);
     }
 }

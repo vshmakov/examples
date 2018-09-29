c1f5db86bd486a2d93fefc094de3db5cd367030f
Vadim Shmakov <shmakovvadim@mail.ru>
2018-09-29 14:36:02 +0500

    Done ExampleRepository refactoring

--- src/Repository/ExampleRepository.php
+++ src/Repository/ExampleRepository.php
@@ -2,129 +2,162 @@
 
 namespace App\Repository;
 
-use App\Entity\Example;
+
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Symfony\Bridge\Doctrine\RegistryInterface;
-use App\Service\ExampleManager as ExMNG;
+use App\Service\ExampleManager;
 use App\Service\UserLoader;
+use App\Entity\Example;
+use App\Entity\Attempt;
+use App\Entity\User;
 
 class ExampleRepository extends ServiceEntityRepository
 {
     use BaseTrait;
-    private $exMng;
-    private $ul;
+    private $exampleManager;
+    private $userLoader;
 
-    public function __construct(RegistryInterface $registry, ExMng $m, UserLoader $ul)
+    public function __construct(RegistryInterface $registry, ExampleManager $exampleManager, UserLoader $userLoader)
     {
         parent::__construct($registry, Example::class);
-        $this->exMng = $m;
-        $this->ul = $ul;
+        $this->exampleManager = $exampleManager;
+        $this->userLoader = $userLoader;
     }
 
-    public function findLastUnansweredByAttempt($att)
+    public function findLastUnansweredByAttempt(Attempt $attempt)
     {
-        return $this->v($this->q('select e from App:Example e
+        return $this->getValue(
+            $this->createQuery('select e from App:Example e
 where e.attempt = :a and e.answer is null
 order by e.addTime desc')
-->setParameter('a', $att));
+                ->setParameter('a', $attempt)
+        );
     }
 
-    public function findLastByAttempt($att)
+    public function findLastByAttempt(Attempt $attempt)
     {
-        return $this->v($this->q('select e from App:Example e
+        return $this->getValue(
+            $this->createQuery('select e from App:Example e
 where e.attempt = :a 
 order by e.addTime desc')
-->setParameter('a', $att));
+                ->setParameter('a', $attempt)
+        );
     }
 
-    public function getErrorNum($ex)
+    public function getErrorNum(Example $example)
     {
-        if (false !== $ex->isRight()) {
+        if (false !== $example->isRight()) {
             return;
         }
 
-        return $this->v($this->q('select count(e) from App:Example e
+        return $this->getValue(
+            $this->createQuery('select count(e) from App:Example e
 where e.attempt = :a and e.isRight = false and e.addTime <= :dt')
-->setParameters(['a' => $ex->getAttempt(), 'dt' => $ex->getAddTime()]));
+                ->setParameters(['a' => $example->getAttempt(), 'dt' => $example->getAddTime()])
+        );
     }
 
-    public function getNumber($ex)
+    public function getNumber(Example $example)
     {
-        $wh = $ex->getAttempt()->getSettings()->isDemanding() ? ' and e.isRight != false' : '';
+        $where = $example->getAttempt()->getSettings()->isDemanding() ? ' and e.isRight != false' : '';
 
-        return $this->v($this->q("select count(e) from App:Example e
+        return $this->getValue(
+            $this->createQuery("select count(e) from App:Example e
 where e.attempt = :a and e.addTime < :dt $wh")
-->setParameters(['a' => $ex->getAttempt(), 'dt' => $ex->getAddTime()])) + 1;
+                ->setParameters([
+                    'a' => $example->getAttempt(),
+                    'dt' => $example->getAddTime()
+                ])
+        ) + 1;
     }
 
-    public function findLastUnansweredByAttemptOrGetNew($att)
+    public function findLastUnansweredByAttemptOrGetNew(Attempt $attempt)
     {
-        return $this->findLastUnansweredByAttempt($att) ?? $this->getNew($att);
+        return $this->findLastUnansweredByAttempt($attempt) ?? $this->getNew($attempt);
     }
 
-    public function getNew($att)
+    public function getNew(Attempt $attempt)
     {
-        $ex = (new Example())->setAttempt($att);
+        $example = (new Example)
+            ->setAttempt($attempt);
+        
+            $lastExample = $this->findLastByAttempt($attempt);
 
-        if ($att->getSettings()->isDemanding() && ($l = $this->findLastByAttempt($att)) && !$l->isRight()) {
-            $ex->setFirst($l->getFirst())->setSecond($l->getSecond())->setSign($l->getSign());
+            if ($attempt->getSettings()->isDemanding() && $lastExample && !$lastExample->isRight()) {
+            $example->setFirst($lastExample->getFirst())
+                ->setSecond($lastExample->getSecond())
+                ->setSign($lastExample->getSign());
         } else {
-            ($set = $att->getSettings()->getData());
-            $m = $this->exMng;
-            $s = $m->getRandSign($set);
-            $exs = $this->q('select e from App:Example e
+            ($settings = $attempt->getSettings()->getData());
+            $exampleManager = $this->exampleManager;
+            $sign = $exampleManager->getRandSign($settings);
+            $previousExamples = $this->createQuery('select e from App:Example e
 join e.attempt a
 join a.session s
 join s.user u
 where u = :u and a.addTime > :dt')
-->setParameters(['u' => $this->ul->getUser(), 'dt' => (new \DateTime())->sub(new \DateInterval('P3D'))])
-->getResult();
-            $d = $m->getRandEx($s, $set, ($exs));
-            $ex->setFirst($d->first)->setSecond($d->second)->setSign($d->sign);
-        }
-        $em = $this->em();
-        $em->persist($ex);
-        $em->flush();
+                ->setParameters([
+                    'u' => $this->userLoader->getUser(),
+                    'dt' => (new \DateTime)->sub(new \DateInterval('P3D'))
+                ])
+                ->getResult();
 
-        return $ex;
+            $exampleData = $exampleManager->getRandEx($sign, $settings, $previousExamples);
+            $example->setFirst($exampleData->first)
+                ->setSecond($exampleData->second)
+                ->setSign($exampleData->sign);
+        }
+
+        $entityManager = $this->getEntityManager();
+        $entityManager->persist($example);
+        $entityManager->flush();
+
+        return $example;
     }
 
-    public function getSolvingTime($ex)
+    public function getSolvingTime(Example $example)
     {
-        $s = null;
+        $solvingTime = null;
 
-        if ($ex->getAnswerTime()) {
-            $p = $this->v($this->q('select e from App:Example e
+        if ($example->getAnswerTime()) {
+            $previousExample = $this->getValue(
+                $this->createQuery('select e from App:Example e
 where e.attempt = :att and e.addTime < :dt
 order by e.addTime desc')
-->setParameters(['dt' => $ex->getAddTime(), 'att' => $ex->getAttempt()]));
-
-            $f = $p ? $p->getAnswerTime() : $ex->getAttempt()->getAddTime();
-            $s = $ex->getAnswerTime()->getTimestamp() - $f->getTimestamp();
+                    ->setParameters([
+                        'dt' => $example->getAddTime(),
+                        'att' => $example->getAttempt()
+                    ])
+            );
+            $previousTime = $previousExample ? $previousExample->getAnswerTime() : $example->getAttempt()->getAddTime();
+            $solvingTime = $example->getAnswerTime()->getTimestamp() - $previousTime->getTimestamp();
         }
 
-        return $this->dts($s);
+        return $this->dts($solvingTime);
     }
 
-    public function findByUser($u)
+    public function findByUser(User $user)
     {
-        return $this->q('select e from App:Example e
+        return $this->createQuery('select e from App:Example e
 join e.attempt a
 join a.session s
 where s.user = :u')
-->setParameter('u', $u)
-->getResult();
+            ->setParameter('u', $user)
+            ->getResult();
     }
 
-    public function getUserNumber($ex)
+    public function getUserNumber(Example $example)
     {
-        return $this->v(
-$this->q('select count(e) from App:Example e
+        return $this->getValue(
+            $this->createQuery('select count(e) from App:Example e
 join e.attempt a
 join a.session s
 join s.user u
 where u = :u and e.addTime < :dt and (e.isRight != false or e.isRight is null)')
-->setParameters(['u' => $ex->getAttempt()->getSession()->getUser(), 'dt' => $ex->getAddTime()])
-) + 1;
+                ->setParameters([
+                    'u' => $example->getAttempt()->getSession()->getUser(),
+                    'dt' => $example->getAddTime()
+                ])
+        ) + 1;
     }
 }

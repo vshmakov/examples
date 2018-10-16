7bbca5f98572ea956ef08f1e91bca545c0a41cb5
Vadim Shmakov <shmakovvadim@mail.ru>
2018-10-16 18:31:17 +0500

    Edited PerformanceMeter

--- src/Utils/PerformanceMeter.php
+++ src/Utils/PerformanceMeter.php
@@ -8,72 +8,83 @@ use Psr\Container\ContainerInterface;
 class PerformanceMeter
 {
     private $data;
+    private $states;
     private $isDevelopmentEnvironment;
 
     public function __construct(ContainerInterface $container)
     {
         $this->data = new ArrayCollection();
+        $this->states = new ArrayCollection;
         $environment = $container->getParameter('app_env');
         $this->isDevelopmentEnvironment = 'dev' == $environment;
     }
 
-    public function start(string $key): self
+    public function start(string $key) : self
     {
         if (!$this->isDevelopmentEnvironment) {
             return $this;
         }
 
         if (!$this->data->offsetExists($key)) {
-            $this->data->set($key, new ArrayCollection());
+            $this->data->set($key, new ArrayCollection);
+            $this->states->set($key, 'finished');
+        }
+
+        if ($this->states->get($key) != 'finished') {
+            throw new \LogicException(sprintf(
+                'The %s key das not finished',
+                $key
+            ));
         }
 
         $moments = new ArrayCollection(['start' => $this->getTime()]);
         $this->data->get($key)
             ->add($moments);
+        $this->states->set($key, 'started');
 
         return $this;
     }
 
-    public function finish(string $key): self
+    public function finish(string $key) : self
     {
         if (!$this->isDevelopmentEnvironment) {
             return $this;
         }
 
-        if (!($momentsList = $this->data->get($key))
-            or !($moments = $momentsList->last())
-            or !$moments->offsetExists('start')
-            or $moments->offsetExists('finish')) {
-            throw new \Exception(sprintf(
+        if ($this->states->get($key) != 'started') {
+            throw new \LogicException(sprintf(
                 'Attempted finished %1$s key, but it has not started',
                 $key
             ));
         }
 
-        $moments->set('finish', $this->getTime());
+        $this->data->get($key)->last()->set('finish', $this->getTime());
+        $this->states->set($key, 'finished');
 
         return $this;
     }
 
-    public function getData(): array
+    public function getData() : array
     {
         $result = array_reduce(
             $this->data->toArray(),
             function ($result, $momentsList) {
                 $key = $this->data->indexOf($momentsList);
+                $momentsListState = $this->states->get($key);
+
+                if ($momentsListState && $momentsListState != 'finished') {
+                    throw new \LogicException(sprintf(
+                        'The %s key das not finished',
+                        $key
+                    ));
+                }
 
                 $result[$key] = [
+                    'key' => $key,
                     'calledCount' => $momentsList->count(),
-                    'totalExecutionTime' => (int) array_reduce(
+                    'totalExecutionTime' => (int)array_reduce(
                         $momentsList->toArray(),
                         function ($totalExecutionTime, $moments) use ($key) {
-                            if (!$moments->offsetExists('finish')) {
-                                throw new \Exception(sprintf(
-                                    'The %s key das not finished',
-                                    $key
-                                ));
-                            }
-
                             return $totalExecutionTime + ($moments->get('finish') - $moments->get('start')) * 1000;
                         },
                         0
@@ -90,6 +101,6 @@ class PerformanceMeter
 
     private function getTime()
     {
-        return (float) time() + (float) microtime();
+        return (float)time() + (float)microtime();
     }
 }

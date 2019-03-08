c3145990c8c84a8d2797dc58dd3ca177d0404e8a
Vadim Shmakov <shmakovvadim@mail.ru>
2019-03-08 16:59:49 +0500

    [profile] Done ProfileTypeTest

--- tests/Unit/Form/ProfileTypeTest.php
+++ tests/Unit/Form/ProfileTypeTest.php
@@ -73,11 +73,71 @@ final class ProfileTypeTest extends TestCase
             'subSMax' => 5,
             'subMin' => 7,
             'subMax' => 1,
-            ]);
+        ]);
         $this->normalizeSolveSettings($this->profileType, $profile);
 
         $this->assertSame(5, $profile->getSubMin());
         $this->assertSame(5, $profile->getSubMax());
+    }
+
+    /**
+     * @test
+     */
+    public function eventListenerNormalisesMultiplicationSolveSettings(): void
+    {
+        /** @var Profile $profile */
+        $profile = ObjectAccessor::initialize(Profile::class, [
+            'multFMin' => 3,
+            'multFMax' => 5,
+            'multSMin' => 3,
+            'multSMax' => 5,
+            'multMin' => 12,
+            'multMax' => 11,
+        ]);
+        $this->normalizeSolveSettings($this->profileType, $profile);
+
+        $this->assertSame(12, $profile->getMultMin());
+        $this->assertSame(12, $profile->getMultMax());
+    }
+
+    /**
+     * @test
+     */
+    public function eventListenerNormalisesDivisionSolveSettings(): void
+    {
+        /** @var Profile $profile */
+        $profile = ObjectAccessor::initialize(Profile::class, [
+            'divFMin' => 15,
+            'divFMax' => 20,
+            'divSMin' => 2,
+            'divSMax' => 5,
+            'divMin' => 0,
+            'divMax' => 50,
+        ]);
+        $this->normalizeSolveSettings($this->profileType, $profile);
+
+        $this->assertSame(3, $profile->getDivMin());
+        $this->assertSame(10, $profile->getDivMax());
+    }
+
+    /**
+     * @test
+     */
+    public function eventListenerNormalisesPercentSettings(): void
+    {
+        /** @var Profile $profile */
+        $profile = ObjectAccessor::initialize(Profile::class, [
+            'addPerc' => 1,
+            'subPerc' => 2,
+            'multPerc' => 0,
+            'divPerc' => 1,
+        ]);
+        $this->profileType->normalizePercentData($this->createFormEvent($profile));
+
+        $this->assertSame(25, $profile->getAddPerc());
+        $this->assertSame(50, $profile->getSubPerc());
+        $this->assertSame(0, $profile->getMultPerc());
+        $this->assertSame(25, $profile->getDivPerc());
     }
 
     private function normalizeSolveSettings(ProfileType $profileType, Profile $profile): void

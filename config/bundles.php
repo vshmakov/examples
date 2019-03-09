bf3ff8f6d1237a1b1d4226870dfe4d90c57ac2d7
Vadim Shmakov <shmakovvadim@mail.ru>
2019-03-09 11:27:26 +0500

    [infrastructure] Required ApiPlatform

--- config/bundles.php
+++ config/bundles.php
@@ -18,4 +18,6 @@ return [
     EasyCorp\Bundle\EasyDeployBundle\EasyDeployBundle::class => ['dev' => true, 'test' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
 ];

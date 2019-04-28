3c7d3d614bd2a8cb72b71da569510fdf7de51e03
Vadim Shmakov <shmakovvadim@mail.ru>
2019-04-28 22:07:30 +0500

    [project] Removed global functions

--- src/Twig/AppExtension.php
+++ src/Twig/AppExtension.php
@@ -13,7 +13,6 @@ use App\Repository\UserRepository;
 use App\Security\User\CurrentUserProviderInterface;
 use Doctrine\Common\Inflector\Inflector;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\PropertyAccess\PropertyAccess;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFilter;
 
@@ -126,49 +125,11 @@ final class AppExtension extends AbstractExtension implements \Twig_Extension_Gl
         ), $parameters);
     }
 
-    public function creationTimeNumber($entity, array $entityList): int
-    {
-        $this->sortByAddTime($entityList);
-
-        return array_search($entity, $entityList, true) + 1;
-    }
-
     public function sortByCreationTime($entityList)
     {
-        usort($entityList, [$this, 'addTimeSorter']);
+        $this->sortByDateTime($entityList, 'addTime');
 
         return $entityList;
-    }
-
-    public function sortProfiles($profiles)
-    {
-        $currentProfile = $this->userLoader->getUser()->getCurrentProfile();
-        usort($profiles, function ($e1, $e2) use ($currentProfile) {
-            if ($currentProfile === $e1) {
-                return -1;
-            }
-
-            if ($currentProfile === $e2) {
-                return 1;
-            }
-
-            return $this->addTimeSorter($e1, $e2);
-        });
-
-        return $profiles;
-    }
-
-    public function fillIp($ip)
-    {
-        if ($ip->getCity() && !$ip->getContinent()) {
-            $ip->setIp($ip->getIp());
-            $this->entityManager->flush();
-        }
-    }
-
-    private function addTimeSorter($e1, $e2)
-    {
-        return addTimeSorter($e1, $e2);
     }
 
     public function sortByDateTime(array $entityList, string $dateTimeProperty): array
@@ -193,65 +154,5 @@ final class AppExtension extends AbstractExtension implements \Twig_Extension_Gl
         });
 
         return $entityList;
-    }
-
-    public function sortTeachers($teachers)
-    {
-        $user = $this->userLoader->getUser();
-        usort($teachers, function ($e1, $e2) use ($user) {
-            if ($user->isUserTeacher($e1)) {
-                return -1;
-            }
-
-            if ($user->isUserTeacher($e2)) {
-                return 1;
-            }
-
-            $s1 = $e1->getStudents()->count();
-            $s2 = $e2->getStudents()->count();
-
-            if ($s1 !== $s2) {
-                return $s1 > $s2 ? -1 : 1;
-            }
-
-            return $this->addTimeSorter($e1, $e2);
-        });
-
-        return $teachers;
-    }
-
-    public function sortStudents($students)
-    {
-        usort($students, function ($e1, $e2) {
-            $a1 = $e1->getAttempts()->last();
-            $a2 = $e2->getAttempts()->last();
-
-            if (!$a1) {
-                return 1;
-            }
-
-            if (!$a2) {
-                return -1;
-            }
-
-            return -1 * addTimeSorter($a1, $a2);
-        });
-
-        return $students;
-    }
-
-    public function propertyFilter($objectOrArray, $property, $default = '-')
-    {
-        $propertyAccessor = PropertyAccess::createPropertyAccessorBuilder()
-            ->enableMagicCall()
-            ->getPropertyAccessor();
-        $value = $objectOrArray ? $propertyAccessor->getValue($objectOrArray, $property) : null;
-
-        return false !== $default ? $value ?: $default : $value;
-    }
-
-    public function getActualHomeworksCount(): int
-    {
-        return $this->taskRepository->countActualHomeworksByCurrentUser();
     }
 }

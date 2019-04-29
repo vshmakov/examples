0509e0481c1c0eededc365894817a0bcecb6f116
Vadim Shmakov <shmakovvadim@mail.ru>
2019-04-29 17:07:21 +0500

    [command] Refactored CronCommand

--- src/Command/CronCommand.php
+++ src/Command/CronCommand.php
@@ -9,9 +9,7 @@ use App\Repository\VisitRepository;
 use App\Service\UserLoader;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
@@ -36,10 +34,6 @@ class CronCommand extends Command
 
     protected function configure()
     {
-        $this
-            ->setDescription('Add a short description for your command')
-            ->addArgument('arg1', InputArgument::OPTIONAL, 'Argument description')
-            ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)

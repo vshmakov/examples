960f9cb6be4e1b0007062d9f51352b36dfc4d61a
Vadim Shmakov <shmakovvadim@mail.ru>
2019-05-01 23:36:49 +0500

    [deploy] Done basic deployment

--- deploy.php
+++ deploy.php
@@ -7,7 +7,7 @@ require 'recipe/common.php';
 inventory('config/prod/hosts.yaml');
 
 set('composer_options', '{{composer_action}} --no-scripts');
-
+set('ssh_multiplexing', false);
 set('symfony/console', '{{bin/php}} {{release_path}}/bin/console');
 
 const  LOGS_DIR = 'var/logs';
@@ -30,6 +30,7 @@ desc('Deploy your project');
 task('deploy', [
     'deploy:info',
     'deploy:prepare',
+    //'deploy:upload-assets',
     'deploy:lock',
     'deploy:release',
     'deploy:update_code',
@@ -38,6 +39,7 @@ task('deploy', [
     'deploy:vendors',
     'deploy:test',
     'deploy:clear_paths',
+    'deploy:build',
     'deploy:symlink',
     'deploy:unlock',
     'cleanup',
@@ -45,15 +47,24 @@ task('deploy', [
 ]);
 
 after('deploy:failed', 'deploy:unlock');
+set('release/composer', 'cd {{release_path}} && {{bin/composer}}');
+task('deploy:build', function (): void {
+    run('{{release/composer}} dump-env prod');
+    run('{{release/composer}} dump-autoload --optimize --no-dev --classmap-authoritative');
+});
+
+task('deploy:upload-assets', function (): void {
+    upload('/C/OSPanel/domains/examples/composer.json', '~/abc.def');
+});
 
 task('deploy:test', function (): void {
     run('{{symfony/console}} doctrine:migrations:migrate -n -e test');
     run('{{symfony/console}} doctrine:schema:validate -e test');
     run('{{symfony/console}} doctrine:fixtures:load -n -e test');
-    run('{{bin/php}} {{release_path}}/bin/phpunit');
+    run('cd {{release_path}} && {{bin/php}} vendor/bin/phpunit');
 });
 
-task('pwd', function () {
-    $result = run('{{bin/php}}   ~/www/test.exmasters/releases/1/vendor/symfony/var-dumper/Resources/functions/dump.php');
-    writeln("$result");
+after('deploy:vendors', 'deploy:post-install');
+task('deploy:post-install', function (): void {
+    run('cd {{release_path}} && {{bin/composer}} run-script auto-scripts');
 });
